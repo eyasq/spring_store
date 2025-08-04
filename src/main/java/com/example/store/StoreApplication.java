cd44a6d7adaa49649a0502abf83e93e0d8ff1f93
eyasq <qeremeyas99@gmail.com>
2025-08-04 14:24:42 +0300

    classes/tables

--- src/main/java/com/example/store/StoreApplication.java
+++ src/main/java/com/example/store/StoreApplication.java
@@ -1,5 +1,6 @@
 package com.example.store;
 
+import com.example.store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,6 +12,9 @@ public class StoreApplication {
 	public static void main(String[] args) {
 
 		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+
+	
+
 
 
 	}
